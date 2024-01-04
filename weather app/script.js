19c0ab3926c1aae3e9f27e99447448cd05e2cfd5
Tejesh Chauhan <tejeshchauhan16@gmail.com>
2024-01-04 16:37:22 +0530

    change

--- weather app/script.js
+++ weather app/script.js
@@ -1,12 +1,12 @@
-// console.log('heloo jiii');
-// const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
+console.log('heloo jiii');
+const API_KEY = "29c018a52a547ce0cb2a1651aa50d497";
 
 async function showWeather(){
 
-//  let latitude = 15.3333;
-//  let longitude = 74.0833;
+ let latitude = 15.3333;
+ let longitude = 74.0833;
 
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m" `);
+    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid={API key}`);
 
     const  data = await response.json();
     console.log('weatherdata' , data);
